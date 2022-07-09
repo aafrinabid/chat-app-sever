1439ca410d42943c414cbc8f5049b720d41634a8
aafrinabid <mohdaafrin@gmail.com>
2022-07-09 10:04:39 +0530

    some works done

--- src/components/UserNameContent.jsx
+++ src/components/UserNameContent.jsx
@@ -5,7 +5,7 @@ function UserNameContent() {
   return (
     <div className={classes.upperpart}>
          <img className={classes.profile} src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80' />
-         <p style={{margin:'0px',textAlign:'start', padding:'15px 0px'}}>username</p>
+         <p style={{margin:'0px',marginLeft:'10px',textAlign:'start', padding:'15px 0px'}}>username</p>
 
     </div>
   )
